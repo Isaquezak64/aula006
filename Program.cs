07b1adf8ba61bfdfcbce78136538a108605ccf04
Isaquezak64 <135741800+Isaquezak64@users.noreply.github.com>
2023-06-06 17:28:20 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula006
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite de 1 a 10");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero>10)
+            {
+                Console.WriteLine("digite um número válido"); Console.ReadKey();
+                Main(null);
+                return;
+            }
+            Console.WriteLine("FIM DO PROGRAMA");
+            Console.ReadKey();
+
+        }
+    }
+}
